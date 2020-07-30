46fca61afbd6ab8fd213f60832076255a01408e5
Trillium <spiteless@gmail.com>
2020-07-30 12:54:00 -0700

    Finished Project

--- media-queiries-afternoon/src/Components/Nav.js
+++ media-queiries-afternoon/src/Components/Nav.js
@@ -24,37 +24,42 @@ class Nav extends React.Component {
     return (
     <nav className="nav-container">
         <nav className="nav-container-inner">
-            <div className="logo">
-                <img className="logo-img" src={require("../media/navbar-logo.svg")} alt=""/>
-            </div>
-            <div className="desktop-nav display-desktop">
-                <ul className="nav-links">  
-                    <li className="nav-link">SERVICES!!!!</li>
-                    <li className="nav-link">PORTFOLIO</li>
-                    <li className="nav-link">ABOUT</li>
-                    <li className="nav-link">TEAM</li>
-                    <li className="nav-link">CONTACT</li>
-                </ul>
+            <div className="col space-between col1">
+                <div className="logo">
+                    <img className="logo-img" src={require("../media/navbar-logo.svg")} alt=""/>
+                </div>
+                <div className="desktop-nav display-desktop">
+                    <ul className="nav-links">  
+                        <li className="nav-link">SERVICES</li>
+                        <li className="nav-link">PORTFOLIO</li>
+                        <li className="nav-link">ABOUT</li>
+                        <li className="nav-link">TEAM</li>
+                        <li className="nav-link">CONTACT</li>
+                    </ul>
+                </div>
+
+                <div className="desktop-nav display-mobile">
+                    <button className="menu-btn hamburger-holder bg-yellow text-uppercase"
+                    onClick={() => toggleDropDown()}>
+                        <h3>Menu</h3>
+                    <div>
+                        <div className="hamburger"></div>
+                        <div className="hamburger"></div>
+                        <div className="hamburger"></div>
+                    </div>
+                    </button>
+                </div>
             </div>
 
-            <div className="desktop-nav display-mobile">
-                <button className="hamburger-holder bg-yellow text-uppercase"
-                onClick={() => toggleDropDown()}>
-                    <h3>Menu</h3>
-                <div>
-                    <div className="hamburger"></div>
-                    <div className="hamburger"></div>
-                    <div className="hamburger"></div>
+                <div className="col col2">
+                    <ul className={"nav-links-dropdown display-mobile " + navDropdown}>  
+                        <li className="nav-link-dropdown">SERVICES</li>
+                        <li className="nav-link-dropdown">PORTFOLIO</li>
+                        <li className="nav-link-dropdown">ABOUT</li>
+                        <li className="nav-link-dropdown">TEAM</li>
+                        <li className="nav-link-dropdown">CONTACT</li>
+                    </ul>
                 </div>
-                </button>
-                <ul className={"nav-links-dropdown display-mobile " + navDropdown}>  
-                    <li className="nav-link">SERVICES</li>
-                    <li className="nav-link">PORTFOLIO</li>
-                    <li className="nav-link">ABOUT</li>
-                    <li className="nav-link">TEAM</li>
-                    <li className="nav-link">CONTACT</li>
-                </ul>
-            </div>
         </nav>
     </nav>
         
