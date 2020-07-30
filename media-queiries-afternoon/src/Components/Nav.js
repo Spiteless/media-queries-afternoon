8d818834c431bd537042dd0530f9301ec634d6a6
Trillium <spiteless@gmail.com>
2020-07-30 11:51:51 -0700

    Perliminary menu dropdown function

--- media-queiries-afternoon/src/Components/Nav.js
+++ media-queiries-afternoon/src/Components/Nav.js
@@ -6,17 +6,48 @@ class Nav extends React.Component {
         this.state = {
             "desktop-nav": true,
             "nav-menu": false,
-            "nav-dropdown": false,
+            "navDropdown": "",
         }
     }
-    render(){ return (
+
+    toggleDropDown = () => {
+        let newDropdownState = (this.state["navDropdown"] === "" ) ? "hide" : ""
+        this.setState({
+            "navDropdown": newDropdownState
+        })
+        console.log(this.state)
+    }
+    render(){
+    const {navDropdown} = this.state
+    const {toggleDropDown} = this
+        
+    return (
     <nav className="nav-container">
         <nav className="nav-container-inner">
             <div className="logo">
                 <img className="logo-img" src={require("../media/navbar-logo.svg")} alt=""/>
             </div>
-            <div className="desktop-nav">
-                <ul className="nav-links">
+            <div className="desktop-nav display-desktop">
+                <ul className="nav-links">  
+                    <li className="nav-link">SERVICES!!!!</li>
+                    <li className="nav-link">PORTFOLIO</li>
+                    <li className="nav-link">ABOUT</li>
+                    <li className="nav-link">TEAM</li>
+                    <li className="nav-link">CONTACT</li>
+                </ul>
+            </div>
+
+            <div className="desktop-nav display-mobile">
+                <button className="hamburger-holder bg-yellow text-uppercase"
+                onClick={() => toggleDropDown()}>
+                    <h3>Menu</h3>
+                <div>
+                    <div className="hamburger"></div>
+                    <div className="hamburger"></div>
+                    <div className="hamburger"></div>
+                </div>
+                </button>
+                <ul className={"nav-links-dropdown display-mobile " + navDropdown}>  
                     <li className="nav-link">SERVICES</li>
                     <li className="nav-link">PORTFOLIO</li>
                     <li className="nav-link">ABOUT</li>
